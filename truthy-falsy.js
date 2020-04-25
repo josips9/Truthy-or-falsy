b4c9e9c64d148585641b00e4e956199a0d6aeb34
Josip Subašić <josip.subasic9@gmail.com>
2020-04-25 11:38:37 +0200

    Added some more functionialities on truthy-falsy.js

--- truthy-falsy.js
+++ truthy-falsy.js
@@ -1,3 +1,23 @@
+const name = 'josip';
+const last = 'subašić';
+
+if(name === 'josip' || (name === 'josip' && last === 'subašić')) {
+    console.log('Cool name bro');
+}
+
+const isAwesomeName = 'awesome'.includes(name);
+if(isAwesomeName) {
+    console.log('Super cool name bro');
+}
+
+function nameIsAwesome = (name) {
+    return 'awesome'.includes(name);
+}
+
+if (nameIsAwesome('josip')) {
+    console.log('Cool name josip')
+}
+
 const values = [[], {}, -10, 1, 0, '', 'full string', ' ',
 undefined, NaN, null,];
 
